37e27701fdf396627b6177a08b2e88e3f6a8f808
vladimir <safonov.open@gmail.com>
2019-12-06 00:03:51 +0600

    Admin module create

---
+++ migrations/m191205_165142_create_article_tag_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%article_tag}}`.
+ */
+class m191205_165142_create_article_tag_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%article_tag}}', [
+            'id' => $this->primaryKey(),
+            'article_id' => $this->integer(),
+            'tag_id' => $this->integer(),
+        ]);
+
+
+        $this->createIndex(
+            'tag_article_article_id',
+            'article_tag',
+            'article_id');
+
+        $this->addForeignKey(
+            'tag_article_article_id',
+            'article_tag',
+            'article_id',
+            'article',
+            'id',
+            'CASCADE',
+            );
+
+        $this->createIndex(
+            'idx_tag_id',
+            'article_tag',
+            'tag_id');
+
+        $this->addForeignKey(
+            'fk-tag_id',
+            'article_tag',
+            'tag_id',
+            'tag',
+            'id',
+            'CASCADE',
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-tag_id', 'article_tag');
+        $this->dropIndex('idx_tag_id', 'article_tag');
+        $this->dropForeignKey('tag_article_article_id', 'article_tag');
+        $this->dropIndex('tag_article_article_id', 'article_tag');
+        $this->dropTable('{{%article_tag}}');
+    }
+}
